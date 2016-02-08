ebe5afee8420c45790ae6e605d7d7b367edebe00
skg113 <skg113@imperial.ac.uk>
2016-02-08 23:06:41 +0000

    Implemented Move Forward and Backward

--- src/Rover.java
+++ src/Rover.java
@@ -2,4 +2,34 @@
  * Created by slav on 16-2-8.
  */
 public class Rover {
+    private char facingDirection;
+    private int x;
+    private int y;
+
+    public Rover(char facingDirection, int x, int y) {
+        this.facingDirection = facingDirection;
+        this.x = x;
+        this.y = y;
+    }
+
+    public char getFacingDirection() {
+        return facingDirection;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void move(char direction){
+        if(direction == 'F'){
+            y += 1;
+        } else {
+            y -= 1;
+        }
+    }
+
 }
--- tests/Rover_Test.java
+++ tests/Rover_Test.java
@@ -1,5 +1,35 @@
 /**
  * Created by slav on 16-2-8.
  */
+import org.junit.*;
 public class Rover_Test {
+
+    Rover rover ;
+    @Test
+    public void checkIfItGoesForward(){
+        rover = new Rover('N', 0, 0);
+        rover.move('F');
+        Assert.assertTrue(rover.getY() == 1);
+    }
+    @Test
+    public void checkIfItGoesBackward(){
+        rover = new Rover('N', 0, 1);
+        rover.move('B');
+        Assert.assertTrue(rover.getY() == 0);
+    }
+    @Test
+    public void checkIfItGoingForwardDirectionIsKept(){
+        rover = new Rover('N', 0, 0);
+        rover.move('F');
+        Assert.assertTrue(rover.getFacingDirection() == 'N');
+    }
+    @Test
+    public void checkIfItGoesBackwardDirectionIdKept(){
+        rover = new Rover('N', 0, 1);
+        rover.move('B');
+        Assert.assertTrue(rover.getFacingDirection() == 'N');
+    }
+
+
+
 }
