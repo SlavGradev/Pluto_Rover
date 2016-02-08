2cfb30332d674835185cf10b98733121d54322ba
skg113 <skg113@imperial.ac.uk>
2016-02-08 23:55:57 +0000

    Implemented Rotation

--- src/Rover.java
+++ src/Rover.java
@@ -24,12 +24,47 @@ public class Rover {
         return y;
     }
 
-    public void move(char direction){
-        if(direction == 'F'){
-            y += 1;
+    public void command(char commandType){
+        if(commandType == 'F' || commandType == 'B'){
+            move(commandType);
         } else {
-            y -= 1;
+            rotate(commandType);
         }
     }
 
+    private void move(char direction){
+        if(direction == 'F'){
+            switch(facingDirection) {
+                case 'E': {x -= 1;break;}
+                case 'N': {y += 1;break;}
+                case 'W': {x += 1;break;}
+                case 'S': {y -= 1;break;}
+            }
+        } else {
+            switch(facingDirection) {
+                case 'E': {x += 1;break;}
+                case 'N': {y -= 1;break;}
+                case 'W': {x -= 1;break;}
+                case 'S': {y += 1;break;}
+            }
+        }
+    }
+
+    private void rotate(char direction) {
+        if(direction == 'L'){
+            switch(facingDirection) {
+                case 'E': {facingDirection = 'S';break;}
+                case 'N': {facingDirection = 'E';break;}
+                case 'W': {facingDirection = 'N';break;}
+                case 'S': {facingDirection = 'W';break;}
+            }
+        } else {
+            switch(facingDirection) {
+                case 'E': {facingDirection = 'N';break;}
+                case 'N': {facingDirection = 'W';break;}
+                case 'W': {facingDirection = 'S';break;}
+                case 'S': {facingDirection = 'E';break;}
+            }
+        }
+    }
 }
--- tests/Rover_Test.java
+++ tests/Rover_Test.java
@@ -4,32 +4,173 @@
 import org.junit.*;
 public class Rover_Test {
 
-    Rover rover ;
+    Rover rover;
+
+    // Movement tests
     @Test
     public void checkIfItGoesForward(){
         rover = new Rover('N', 0, 0);
-        rover.move('F');
+        rover.command('F');
         Assert.assertTrue(rover.getY() == 1);
     }
     @Test
     public void checkIfItGoesBackward(){
         rover = new Rover('N', 0, 1);
-        rover.move('B');
+        rover.command('B');
         Assert.assertTrue(rover.getY() == 0);
     }
     @Test
     public void checkIfItGoingForwardDirectionIsKept(){
         rover = new Rover('N', 0, 0);
-        rover.move('F');
+        rover.command('F');
         Assert.assertTrue(rover.getFacingDirection() == 'N');
     }
     @Test
     public void checkIfItGoesBackwardDirectionIdKept(){
         rover = new Rover('N', 0, 1);
-        rover.move('B');
+        rover.command('B');
+        Assert.assertTrue(rover.getFacingDirection() == 'N');
+    }
+    // Rotation Tests
+    @Test
+    public void checkIfRotatesRight(){
+        rover = new Rover('N', 0, 0);
+        rover.command('R');
+        Assert.assertTrue(rover.getFacingDirection() == 'W');
+    }
+    @Test
+    public void checkIfRotatesLeft(){
+        rover = new Rover('N', 0, 1);
+        rover.command('L');
+        Assert.assertTrue(rover.getFacingDirection() == 'E');
+    }
+
+    @Test
+    public void checkIfRotatesAndGoesForward(){
+        rover = new Rover('N', 0, 0);
+        rover.command('R');
+        rover.command('F');
+        Assert.assertTrue(rover.getFacingDirection() == 'W');
+        Assert.assertTrue(rover.getX() == 1);
+    }
+    @Test
+    public void checkIfRotatesLeftAndGoesBackward(){
+        rover = new Rover('N', 0, 0);
+        rover.command('L');
+        rover.command('B');
+        Assert.assertTrue(rover.getFacingDirection() == 'E');
+        Assert.assertTrue(rover.getX() == 1);
+    }
+
+    @Test
+    public void checkIfMultipleRotationWorks(){
+        rover = new Rover('N', 0, 0);
+        rover.command('R');
+        rover.command('R');
+        Assert.assertTrue(rover.getFacingDirection() == 'S');
+    }
+    @Test
+    public void checkIfMoveForEast(){
+        rover = new Rover('E', 1, 1);
+        rover.command('F');
+        Assert.assertTrue(rover.getX() == 0);
+    }
+    @Test
+    public void checkIfMoveBackEast(){
+        rover = new Rover('E', 1, 1);
+        rover.command('B');
+        Assert.assertTrue(rover.getX() == 2);
+    }
+    @Test
+    public void checkIfMoveForWest(){
+        rover = new Rover('W', 1, 1);
+        rover.command('F');
+        Assert.assertTrue(rover.getX() == 2);
+    }
+    @Test
+    public void checkIfMoveBackWest(){
+        rover = new Rover('W', 1, 1);
+        rover.command('B');
+        Assert.assertTrue(rover.getX() == 0);
+    }
+    @Test
+    public void checkIfMoveForNorth(){
+        rover = new Rover('N', 1, 1);
+        rover.command('F');
+        Assert.assertTrue(rover.getY() == 2);
+    }
+    @Test
+    public void checkIfMoveBackNorth(){
+        rover = new Rover('N', 1, 1);
+        rover.command('B');
+        Assert.assertTrue(rover.getY() == 0);
+    }
+    @Test
+    public void checkIfMoveForSouth(){
+        rover = new Rover('S', 1, 1);
+        rover.command('F');
+        Assert.assertTrue(rover.getY() == 0);
+    }
+    @Test
+    public void checkIfMoveBackSouth(){
+        rover = new Rover('S', 1, 1);
+        rover.command('B');
+        Assert.assertTrue(rover.getY() == 2);
+    }
+
+    @Test
+    public void checkIfRotateLeftNorth(){
+        rover = new Rover('N', 0, 0);
+        rover.command('L');
+        Assert.assertTrue(rover.getFacingDirection() == 'E');
+    }
+
+    @Test
+    public void checkIfRotateRightNorth(){
+        rover = new Rover('N', 0, 0);
+        rover.command('R');
+        Assert.assertTrue(rover.getFacingDirection() == 'W');
+    }
+
+    @Test
+    public void checkIfRotateLeftWest(){
+        rover = new Rover('W', 0, 0);
+        rover.command('L');
         Assert.assertTrue(rover.getFacingDirection() == 'N');
     }
 
+    @Test
+    public void checkIfRotateRightWest(){
+        rover = new Rover('W', 0, 0);
+        rover.command('R');
+        Assert.assertTrue(rover.getFacingDirection() == 'S');
+    }
+    @Test
+    public void checkIfRotateLeftEast(){
+        rover = new Rover('E', 0, 0);
+        rover.command('L');
+        Assert.assertTrue(rover.getFacingDirection() == 'S');
+    }
+
+    @Test
+    public void checkIfRotateRightEast(){
+        rover = new Rover('E', 0, 0);
+        rover.command('R');
+        Assert.assertTrue(rover.getFacingDirection() == 'N');
+    }
+    @Test
+    public void checkIfRotateLeftSouth(){
+        rover = new Rover('S', 0, 0);
+        rover.command('L');
+        Assert.assertTrue(rover.getFacingDirection() == 'W');
+    }
+
+    @Test
+    public void checkIfRotateRightSouth(){
+        rover = new Rover('S', 0, 0);
+        rover.command('R');
+        Assert.assertTrue(rover.getFacingDirection() == 'E');
+    }
 
 
 }
